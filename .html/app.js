8812e50d95058ad5ec3e5297756454d9266f1225
brunda <brunda@gmail.com>
2019-10-01 15:55:16 +0530

    this is my first commit

---
+++ .html/app.js
@@ -0,0 +1,15 @@
+var str="hello javascript";
+var str1="hello nodejs";
+var str3=`hello angular
+hduhdkdk
+dkfjfnejd
+ddjsks
+dhdhs
+<h1>hello</h1>`;
+console.log(str);
+console.log(str1);
+console.log(str3);
+document.write(`
+    my name is ${str} hello
+    `
+);
